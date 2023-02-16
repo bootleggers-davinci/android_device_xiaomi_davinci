20000c3e878a33dfc921768da5a2491f25f73192
Stocco Matis <32925370+Rush-er@users.noreply.github.com>
2023-02-16 15:01:45 +0100

    davinci: adapt for Bootleggers

--- bootleg_davinci.mk
+++ bootleg_davinci.mk
@@ -11,13 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_davinci
+PRODUCT_NAME := bootleg_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9T
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Booleggers specific values
+# BOOTLEGGERS_BUILD_TYPE := Shishufied
+TARGET_BOOTLEG_ARCH := arm64
+WITH_MICROG=false
+WITH_GAPPS=false
+BOOTLEGGERS_BUILD_APPS_BUNDLE=true
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Maintainer Prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bootleggers.maintainer=Matis
